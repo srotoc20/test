a568d5d3f86ac8088bd7c1ce968ca199e621d314
Tobias Schröttner <srotoc20@htl-kaindorf.at>
2023-02-10 09:09:53 +0100

    updated test1.java

--- src/test1.java
+++ src/test1.java
@@ -3,6 +3,6 @@ import java.awt.event.ComponentAdapter;
 public class test1 {
 
     public static void main(String[] args) {
-        System.out.println("dkjdfkhgsdkfhgasd");
+        System.out.println("Test");
     }
 }
